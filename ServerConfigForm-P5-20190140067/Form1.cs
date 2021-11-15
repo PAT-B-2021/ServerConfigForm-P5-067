5327c9f04654276a4f09acb5c9d5f2a59608aa40
Muhammad Fajri Yosri <fajriyosri23@gmail.com>
2021-11-16 00:56:34 +0800

    Add project files.

---
+++ ServerConfigForm-P5-20190140067/Form1.cs
@@ -0,0 +1,43 @@
+﻿using ServiceMtk_P1_20190140067;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_20190140067
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost Host;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            labelOnOrOff.Text = "Server Off";
+            labelKet.Text = "klik ON untuk menjalankan server";
+            Host.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+
+            labelOnOrOff.Text = "Server On";
+            labelKet.Text = "klik off untuk mematikan server";
+            Host.Open();
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+        }
+    }
+}
